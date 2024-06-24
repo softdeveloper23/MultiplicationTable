9c6b4552828d715a51bc66de01b3971f52ea8828
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-24 16:48:21 +0200

    Added code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,16 @@ public class Main {
         System.out.println("Enter a number between 1 and 10: ");
         int number = scanner.nextInt();
         int ten = 10;
-
+        // Checks if user entered a number between 1 and 10
         if (number > 0 && number <= ten) {
-            for (; number < number + ten; number++) {
-
+            // Loop creates a 10x10 multiplication table
+            for (int i = 1; i <= ten; i++) {
+                for (int j = 1; j <= ten; j++) {
+                    System.out.print(number * j + "\t");
+                }
+                number += 1;
+                System.out.println();
             }
         }
-
     }
 }
